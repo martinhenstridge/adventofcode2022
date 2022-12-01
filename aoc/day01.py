6dca11939ed335fe36f4a6495a66a757b99880c8
Martin Henstridge <mchenstridge@gmail.com>
2022-12-01 08:41:32 +0000

    Use iterator rather than list

--- aoc/day01.py
+++ aoc/day01.py
@@ -1,20 +1,15 @@
 def parse_elf_totals(data):
-    elves = []
-
     elf = 0
     for line in data.splitlines():
         if line:
             elf += int(line)
         else:
-            elves.append(elf)
+            yield elf
             elf = 0
-
-    return elves
 
 
 def run(data):
-    totals = parse_elf_totals(data)
-    ordered = sorted(totals, reverse=True)
+    ordered = sorted(parse_elf_totals(data), reverse=True)
 
     top1 = ordered[0]
     top3 = sum(ordered[:3])
