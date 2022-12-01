46e76c2345754a246e17d039eed1dfd94b9ca37d
Martin Henstridge <mchenstridge@gmail.com>
2022-12-01 08:36:18 +0000

    Day 1

--- aoc/day01.py
+++ aoc/day01.py
@@ -1,2 +1,22 @@
+def parse_elf_totals(data):
+    elves = []
+
+    elf = 0
+    for line in data.splitlines():
+        if line:
+            elf += int(line)
+        else:
+            elves.append(elf)
+            elf = 0
+
+    return elves
+
+
 def run(data):
-    return 0, 0
+    totals = parse_elf_totals(data)
+    ordered = sorted(totals, reverse=True)
+
+    top1 = ordered[0]
+    top3 = sum(ordered[:3])
+
+    return top1, top3
