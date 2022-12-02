899c420a7f8b19c00c258ddf3aa95c731156cd8d
Martin Henstridge <mchenstridge@gmail.com>
2022-12-02 10:24:57 +0000

    Day 2

---
+++ aoc/day02.py
@@ -0,0 +1,31 @@
+RESULTS_RPS = {
+    "A X": 1 + 3,
+    "A Y": 2 + 6,
+    "A Z": 3 + 0,
+    "B X": 1 + 0,
+    "B Y": 2 + 3,
+    "B Z": 3 + 6,
+    "C X": 1 + 6,
+    "C Y": 2 + 0,
+    "C Z": 3 + 3,
+}
+
+RESULTS_WLD = {
+    "A X": 3 + 0,
+    "A Y": 1 + 3,
+    "A Z": 2 + 6,
+    "B X": 1 + 0,
+    "B Y": 2 + 3,
+    "B Z": 3 + 6,
+    "C X": 2 + 0,
+    "C Y": 3 + 3,
+    "C Z": 1 + 6,
+}
+
+
+def run(data):
+    rounds = data.splitlines()
+    return (
+        sum(RESULTS_RPS[r] for r in rounds),
+        sum(RESULTS_WLD[r] for r in rounds),
+    )
