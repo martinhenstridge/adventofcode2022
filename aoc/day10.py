c9e4155729f494b359e0dafa301a5e114d4527e5
Martin Henstridge <mchenstridge@gmail.com>
2022-12-10 14:54:55 +0000

    Day 10 streamlined

--- aoc/day10.py
+++ aoc/day10.py
@@ -1,61 +1,33 @@
-import enum
-
-
-class Instruction(enum.Enum):
-    NOOP = 0
-    ADDX = 1
-
-
-def parse_instructions(data):
-    for line in data.splitlines():
-        if line == "noop":
-            yield Instruction.NOOP, 0
-        elif line.startswith("addx"):
-            yield Instruction.ADDX, int(line[5:])
-
-
 def run_cpu(instructions):
     x = 1
-    cycle = 1
+    xs = [x]
 
-    for instruction, value in instructions:
-        if instruction is Instruction.NOOP:
-            yield cycle, x
-            cycle += 1
-        elif instruction is Instruction.ADDX:
-            yield cycle, x
-            cycle += 1
-            yield cycle, x
-            cycle += 1
-            x += value
-
-
-def signal_strengths(instructions):
-    for cycle, x in run_cpu(instructions):
-        if cycle in (20, 60, 100, 140, 180, 220):
-            yield cycle * x
-
-
-def draw_crt(instructions):
-    drawing = 0
-    print()
-
-    for cycle, x in run_cpu(instructions):
-        if (drawing == x - 1) or (drawing == x) or (drawing == x + 1):
-            print(" #", end="")
+    for instruction in instructions:
+        if instruction == "noop":
+            xs.append(x)
         else:
-            print(" .", end="")
+            xs.append(x)
+            xs.append(x)
+            x += int(instruction[5:])
 
-        drawing += 1
-        if drawing == 40:
-            drawing = 0
-            print()
+    return xs
 
 
 def run(data):
-    instructions = [i for i in parse_instructions(data)]
+    instructions = data.splitlines()
+    xs = run_cpu(instructions)
 
-    sum_signal_strengths = sum(ss for ss in signal_strengths(instructions))
-    draw_crt(instructions)
+    sum_signal_strengths = sum(
+        cycle * xs[cycle] for cycle in (20, 60, 100, 140, 180, 220)
+    )
+
+    print()
+    for start in range(1, 240, 40):
+        print(
+            " ".join(
+                "#" if x - 1 <= cycle <= x + 1 else "."
+                for cycle, x in enumerate(xs[start : start + 40])
+            )
+        )
 
     return sum_signal_strengths, "RZHFGJCB"
