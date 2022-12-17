b7336af68a0b5ed846051ce206a872f6a0e067ee
Martin Henstridge <mchenstridge@gmail.com>
2022-12-17 15:37:39 +0000

    Day 17 part 2

--- aoc/day17.py
+++ aoc/day17.py
@@ -1,3 +1,6 @@
+import itertools
+
+# Coordinates of rock parts: real is x, imaginary is y.
 SHAPES = (
     (0 + 0j, 1 + 0j, 2 + 0j, 3 + 0j),
     (1 + 0j, 0 + 1j, 1 + 1j, 2 + 1j, 1 + 2j),
@@ -7,44 +10,59 @@ SHAPES = (
 )
 
 
-def fall(chamber, wind_iter, rock):
+def simulate(chamber, highest, wind_iter, shape_iter):
+    rock = [p + complex(2, highest + 3) for p in next(shape_iter)]
+
     while True:
-        # Wind blows rock sideways
         wind = next(wind_iter)
         rock_next = [p - 1 if wind == "<" else p + 1 for p in rock]
         if all(0 <= p.real <= 6 and p not in chamber for p in rock_next):
             rock = rock_next
 
-        # Rock falls downwards
-        rock_next = [p + complex(0, -1) for p in rock]
+        rock_next = [p - 1j for p in rock]
         if any(p.imag < 0 or p in chamber for p in rock_next):
-            return rock
+            break
         rock = rock_next
 
+    chamber.update(rock)
+    highest = max(1 + max(p.imag for p in rock), highest)
 
-def cycle(data):
-    i = 0
-    while True:
-        yield data[i]
-        i += 1
-        if i == len(data):
-            i = 0
+    return chamber, highest
+
+
+def calculate_height(history, offset, period, iterations):
+    loops = (iterations - offset) // period
+    index = (iterations - offset) - (loops * period)
+    return history[index] + loops * (history[-1] - history[0])
 
 
 def run(data):
     chamber = set()
     highest = 0
 
-    shape_iter = cycle(SHAPES)
-    wind_iter = cycle(data)
+    wind_iter = itertools.cycle(data)
+    shape_iter = itertools.cycle(SHAPES)
 
-    for _ in range(2022):
-        rock = fall(
-            chamber,
-            wind_iter,
-            [p + complex(2, highest + 3) for p in next(shape_iter)],
-        )
-        chamber.update(rock)
-        highest = max(1 + max(p.imag for p in rock), highest)
+    # From experimentation, the simulation loops exactly in sync with
+    # the air jets. The first loop takes 1728 rocks and then each
+    # subsequent loop takes 1735 rocks (assuming the difference here is
+    # due to the initial loop starting from a completely flat floor and
+    # subsequent loops starting from some other conformation).
+    offset = 1728
+    period = 1735
 
-    return int(highest), 0
+    # Reach beginning of cycle
+    for _ in range(offset):
+        chamber, highest = simulate(chamber, highest, wind_iter, shape_iter)
+
+    # Run through a complete cycle, collecting height history
+    history = []
+    for _ in range(period):
+        chamber, highest = simulate(chamber, highest, wind_iter, shape_iter)
+        history.append(int(highest))
+
+    # Extrapolate height for the requested rock counts.
+    part1 = calculate_height(history, offset, period, 2022 - 1)
+    part2 = calculate_height(history, offset, period, 1000000000000 - 1)
+
+    return part1, part2
