93a00c7fe13f5608ba47ca823cf387ce23531436
Martin Henstridge <mchenstridge@gmail.com>
2022-12-22 22:55:05 +0000

    Day 22, part 2

--- aoc/day22.py
+++ aoc/day22.py
@@ -58,92 +58,80 @@ def oob_wrap(board, row, col, facing):
 
 
 def oob_cube(board, row, col, facing):
+    # The input shape is as follows:
+    #
+    #           __a__ __b__
+    #          |     |     |
+    #          n     |     c
+    #          |_____|__d__|
+    #          |     |
+    #          m     e
+    #     __l__|_____|
+    #    |     |     |
+    #    k     |     f
+    #    |_____|__g__|
+    #    |     |
+    #    j     h
+    #    |__i__|
+    #
     if facing is Facing.RIGHT:
-        # 3 -> 6
+        # c -> f
         if 1 <= row <= 50:
-            print("3 -> 6")
-            assert col == 151
             return 151 - row, 100, Facing.LEFT
 
-        # 5 -> 4
+        # e -> d
         if 51 <= row <= 100:
-            print("5 -> 4")
-            assert col == 101
             return 50, row + 50, Facing.UP
 
-        # 6 -> 3
+        # f -> c
         if 101 <= row <= 150:
-            print("6 -> 3")
-            assert col == 101
             return 151 - row, 150, Facing.LEFT
 
-        # 8 -> 7
+        # h -> g
         if 151 <= row <= 200:
-            print("8 -> 7")
-            assert col == 51
             return 150, row - 100, Facing.UP
 
     if facing is Facing.DOWN:
-        # 9 -> 2
+        # i -> b
         if 1 <= col <= 50:
-            print("9 -> 2")
-            assert row == 201
             return 1, col + 100, Facing.DOWN
 
-        # 7 -> 8
+        # g -> h
         if 51 <= col <= 100:
-            print("7 -> 8")
-            assert row == 151
             return col + 100, 50, Facing.LEFT
 
-        # 4 -> 5
+        # d -> e
         if 101 <= col <= 150:
-            print("4 -> 5")
-            assert row == 51
             return col - 50, 100, Facing.LEFT
 
     if facing is Facing.LEFT:
-        # 14 -> 11
+        # n -> k
         if 1 <= row <= 50:
-            print("14 -> 11")
-            assert col == 50
-            return 151 - row, 1, Facing.LEFT
+            return 151 - row, 1, Facing.RIGHT
 
-        # 13 -> 12
+        # m -> l
         if 51 <= row <= 100:
-            print("13 -> 12")
-            assert col == 50
             return 101, row - 50, Facing.DOWN
 
-        # 11 -> 14
+        # k -> n
         if 101 <= row <= 150:
-            print("11 -> 14")
-            assert col == 0
             return 151 - row, 51, Facing.RIGHT
 
-        # 10 -> 1
+        # j -> a
         if 151 <= row <= 200:
-            print("10 -> 1")
-            assert col == 0
             return 1, row - 100, Facing.DOWN
 
     if facing is Facing.UP:
-        # 12 -> 13
+        # l -> m
         if 1 <= col <= 50:
-            print("12 -> 13")
-            assert row == 100
             return col + 50, 51, Facing.RIGHT
 
-        # 1 -> 10
+        # a -> j
         if 51 <= col <= 100:
-            print("1 -> 10")
-            assert row == 0
             return col + 100, 1, Facing.RIGHT
 
-        # 2 -> 9
+        # b -> i
         if 101 <= col <= 150:
-            print("2 -> 9")
-            assert row == 0
             return 200, col - 100, Facing.UP
 
     assert False
@@ -155,16 +143,10 @@ def move(board, row, col, facing, count, oob):
         r, c = next_tile(board, row, col, facing)
 
         if board[r][c] == " ":
-            print("heading oob:", r, c, f)
             r, c, f = oob(board, r, c, f)
-            print("arrived at:", r, c, f)
-            assert 0 <= r < len(board)
-            assert 0 <= c < len(board[0])
-            assert board[r][c] != " "
 
         if board[r][c] == "#":
-            pass
-            #break
+            break
 
         if board[r][c] == ".":
             row, col, facing = r, c, f
@@ -172,15 +154,10 @@ def move(board, row, col, facing, count, oob):
     return row, col, facing
 
 
-def calculate_password(row, col, facing):
-    return 1000 * row + 4 * col + facing
-
-
 def follow_instructions(board, instructions, oob):
     row = 1
     col = board[row].find(".")
     facing = Facing.RIGHT
-    print("start", row, col, facing)
 
     for instruction in instructions:
         if instruction == "L":
@@ -189,79 +166,14 @@ def follow_instructions(board, instructions, oob):
             facing = Facing((facing + 1) % 4)
         else:
             row, col, facing = move(board, row, col, facing, instruction, oob)
-        print("instruction", instruction, row, col, facing)
 
-    print("finish", row, col, facing)
-    return calculate_password(row, col, facing)
+    return 1000 * row + 4 * col + facing
 
 
 def run(data):
     board, instructions = parse_board_instructions(data)
 
-    follow_instructions(board, ["R", 200], oob_cube)
-    #follow_instructions(board, ["R", "R", "R", 200], oob_cube)
-
-    #password1 = follow_instructions(board, instructions, oob_wrap)
-    #password2 = follow_instructions(board, instructions, oob_cube)
-    password1 = 0
-    password2 = 0
+    password1 = follow_instructions(board, instructions, oob_wrap)
+    password2 = follow_instructions(board, instructions, oob_cube)
 
     return password1, password2
-
-"""
-# 1 -> 10
-assert oob_cube(None, 0, 51, Facing.UP) == (151, 1, Facing.RIGHT)
-assert oob_cube(None, 0, 100, Facing.UP) == (200, 1, Facing.RIGHT)
-
-# 2 -> 9
-assert oob_cube(None, 0, 101, Facing.UP) == (200, 1, Facing.UP)
-assert oob_cube(None, 0, 150, Facing.UP) == (200, 50, Facing.UP)
-
-# 3 -> 6
-assert oob_cube(None, 1, 151, Facing.RIGHT) == (150, 100, Facing.LEFT)
-assert oob_cube(None, 50, 151, Facing.RIGHT) == (101, 100, Facing.LEFT)
-
-# 4 -> 5
-assert oob_cube(None, 51, 101, Facing.DOWN) == (51, 100, Facing.LEFT)
-assert oob_cube(None, 51, 150, Facing.DOWN) == (100, 100, Facing.LEFT)
-
-# 5 -> 4
-assert oob_cube(None, 51, 101, Facing.RIGHT) == (50, 101, Facing.UP)
-assert oob_cube(None, 100, 101, Facing.RIGHT) == (50, 150, Facing.UP)
-
-# 6 -> 3
-assert oob_cube(None, 51, 101, Facing.RIGHT) == (50, 101, Facing.UP)
-assert oob_cube(None, 100, 101, Facing.RIGHT) == (50, 150, Facing.UP)
-
-# 7 -> 8
-assert oob_cube(None, 151, 51, Facing.DOWN) == (151, 50, Facing.LEFT)
-assert oob_cube(None, 151, 100, Facing.DOWN) == (200, 50, Facing.LEFT)
-
-# 8 -> 7
-assert oob_cube(None, 151, 51, Facing.RIGHT) == (150, 51, Facing.UP)
-assert oob_cube(None, 200, 51, Facing.RIGHT) == (150, 100, Facing.UP)
-
-# 9 -> 2
-assert oob_cube(None, 201, 1, Facing.DOWN) == (1, 101, Facing.DOWN)
-assert oob_cube(None, 201, 50, Facing.DOWN) == (1, 150, Facing.DOWN)
-
-# 10 -> 1
-assert oob_cube(None, 151, 0, Facing.LEFT) == (1, 51, Facing.DOWN)
-assert oob_cube(None, 200, 0, Facing.LEFT) == (1, 100, Facing.DOWN)
-
-# 11 -> 14
-assert oob_cube(None, , , Facing.) == (, , Facing.)
-assert oob_cube(None, , , Facing.) == (, , Facing.)
-
-# 12 -> 13
-assert oob_cube(None, , , Facing.) == (, , Facing.)
-assert oob_cube(None, , , Facing.) == (, , Facing.)
-
-# 13 -> 12
-assert oob_cube(None, , , Facing.) == (, , Facing.)
-assert oob_cube(None, , , Facing.) == (, , Facing.)
-
-# 14 -> 11
-assert oob_cube(None, , , Facing.) == (, , Facing.)
-assert oob_cube(None, , , Facing.) == (, , Facing.)
-"""
