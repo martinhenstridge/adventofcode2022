d23c8ad6872b943a8109cb3e0fd957102e681aad
Martin Henstridge <mchenstridge@gmail.com>
2022-12-24 15:58:22 +0000

    Day 16, part 1

--- aoc/day16.py
+++ aoc/day16.py
@@ -21,23 +21,47 @@ def parse_valves(data):
     return rates, links
 
 
+def search(rates, options, current_valve, open_valves, time_remaining):
+    if time_remaining <= 0:
+        return 0
+
+    best = 0
+    for v, t in options[current_valve].items():
+        if v in open_valves:
+            continue
+        total = (time_remaining - t) * rates[v] + search(
+            rates, options, v, open_valves | frozenset([v]), time_remaining - t
+        )
+        if total > best:
+            best = total
+    return best
+
+
 def run(data):
     rates, links = parse_valves(data)
 
+    # Use Floyd-Warshall algorithm to find shortest path from each valve
+    # to every other valve.
     valves = tuple(rates)
     for k in valves:
         for i in valves:
             for j in valves:
                 links[i, j] = min(links[i, j], links[i, k] + links[k, j])
 
-    ds = {}
+    # Transform shortest path data into a more convenient format, and
+    # increment each value by 1 to account for the time taken to open
+    # the valve.
+    options = collections.defaultdict(dict)
     for (a, b), d in links.items():
-        if (a == "AA" or rates[a] > 0) and (b == "AA" or rates[b] > 0):
-            ds[a, b] = d
+        if (
+            a != b
+            and b != "AA"
+            and (a == "AA" or rates[a] > 0)
+            and (b == "AA" or rates[b] > 0)
+        ):
+            options[a][b] = d + 1
 
-    for k, v in ds.items():
-        print(k, v)
+    best1 = search(rates, options, "AA", frozenset(), 30)
+    best2 = 0
 
-    print(len(links), len(ds))
-
-    return 0, 0
+    return best1, best2
