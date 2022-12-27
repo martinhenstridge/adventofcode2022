f5ef710c4fe59847ad59f49c6ca643e0cc6284b6
Martin Henstridge <mchenstridge@gmail.com>
2022-12-27 12:07:51 +0000

    Day 12, Dijkstra faster than A*

--- aoc/day12.py
+++ aoc/day12.py
@@ -1,18 +1,6 @@
 import math
 import heapq
-import functools
-
-
-def memoize(fn):
-    cache = {}
-
-    @functools.wraps(fn)
-    def wrapped(common, key):
-        if key not in cache:
-            cache[key] = fn(common, key)
-        return cache[key]
-
-    return wrapped
+import collections
 
 
 def parse_heightmap(data):
@@ -34,7 +22,6 @@ def parse_heightmap(data):
     return grid, start, end
 
 
-@memoize
 def find_candidates(grid, position):
     x, y = position
     h = grid[position]
@@ -66,13 +53,8 @@ def find_candidates(grid, position):
     return candidates
 
 
-def manhattan(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
-
-def find_shortest_path(grid, end, start):
-    candidates = {p: find_candidates(grid, p) for p in grid}
-    fewest = {p: math.inf for p in grid}
+def find_shortest_path(candidates, end, start):
+    fewest = collections.defaultdict(lambda: math.inf)
     fewest[start] = 0
 
     pending = []
@@ -83,22 +65,22 @@ def find_shortest_path(grid, end, start):
         if p == end:
             return fewest[end]
 
-        for c in find_candidates(grid, p):
+        for c in candidates[p]:
             distance = fewest[p] + 1
             if distance < fewest[c]:
                 fewest[c] = distance
-                priority = distance + manhattan(c, end)
-                heapq.heappush(pending, (priority, c))
+                heapq.heappush(pending, (distance, c))
 
     return math.inf
 
 
 def run(data):
     grid, start, end = parse_heightmap(data)
+    candidates = {p: find_candidates(grid, p) for p in grid}
 
-    from_start = find_shortest_path(grid, end, start)
+    from_start = find_shortest_path(candidates, end, start)
     from_any_a = min(
-        find_shortest_path(grid, end, p) for p, h in grid.items() if h == ord("a")
+        find_shortest_path(candidates, end, p) for p, h in grid.items() if h == ord("a")
     )
 
     return from_start, from_any_a
